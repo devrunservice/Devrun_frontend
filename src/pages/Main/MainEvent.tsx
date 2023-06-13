93d0fad179105dd11cbd8ff467c8985564a6c31d
Ha neul <skyhn0207@naver.com>
2023-06-13 22:59:52 +0900

    feat:메인, 메인이벤트 UI 생성

---
+++ src/pages/Main/MainEvent.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const MainEvent = () => {
+  return (
+    <EventBanner>
+      이벤트 swiper (임시)
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+    </EventBanner>
+  )
+}
+
+const EventBanner = styled.div`
+  border: 1px solid #000;
+  margin-bottom: 50px;
+`
+
+export default MainEvent
