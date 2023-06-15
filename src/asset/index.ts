9fa8a2d73b9e98da0ba760eb727f7287f50ff89a
limhyejin <hejin8307@naver.com>
2023-06-15 22:07:57 +0900

    Chore: SignupForm 코드 정리 (#9)

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -1,8 +1,9 @@
-export { ReactComponent as SubAdv1} from "./SubAdv1.svg";
-export { ReactComponent as MainVisual} from "./MainVisual.svg";
-export { ReactComponent as Delete } from "./Delete.svg";
-export { ReactComponent as Arrow } from "./Arrow.svg";
-export { ReactComponent as Coupon } from "./Coupon.svg";
-export { ReactComponent as FooterLogo } from "./FooterLogo.svg";
-
-
+export {ReactComponent as SubAdv1} from './SubAdv1.svg';
+export {ReactComponent as MainVisual} from './MainVisual.svg';
+export {ReactComponent as Delete} from './Delete.svg';
+export {ReactComponent as Arrow} from './Arrow.svg';
+export {ReactComponent as Coupon} from './Coupon.svg';
+export {ReactComponent as FooterLogo} from './FooterLogo.svg';
+export {ReactComponent as Kakao} from './images/kakao.svg';
+export {ReactComponent as Google} from './images/google.svg';
+export {ReactComponent as Naver} from './images/naver.svg';
