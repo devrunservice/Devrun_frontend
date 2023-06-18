c44ebbceb9d953551d576e841eaac77a029d24e8
hyejin <hejin8307@naver.com>
2023-06-18 16:53:07 +0900

    Design: 회원가입 & 로그인 UI (#9)

--- src/components/SignupForm/SignupForm.tsx
+++ src/components/SignupForm/SignupForm.tsx
@@ -1,5 +1,7 @@
 import {Kakao, Naver, Google} from 'asset';
 import * as St from './styles';
+import {Input} from 'style/Common';
+import PasswordInput from 'components/Login/PasswordInput/PasswordInput';
 
 const SignupForm = () => {
   return (
@@ -9,51 +11,47 @@ const SignupForm = () => {
         <form>
           <St.InputField>
             <St.P>아이디</St.P>
-            <St.Input type="text" placeholder="영문, 숫자 5-11자" />
+            <Input type="text" placeholder="영문, 숫자 5-11자" />
           </St.InputField>
           <St.InputField>
             <St.P>비밀번호</St.P>
-            <St.Input
-              type="password"
-              placeholder="숫자, 영문, 특수문자 조합 최소 8자"
-            />
-            <St.Input type="password" placeholder="비밀번호 재입력" />
+            <PasswordInput placeholder="숫자, 영문, 특수문자 조합 최소 8자" />
+            <PasswordInput placeholder="비밀번호 재입력" />
           </St.InputField>
           <St.InputField>
             <St.P>이름</St.P>
-            <St.Input type="text" placeholder="홍길동" />
+            <Input type="text" placeholder="홍길동" />
           </St.InputField>
           <St.InputField>
             <St.P>이메일</St.P>
-            <St.Input type="email" placeholder="이메일" />
+            <Input type="email" placeholder="이메일" />
           </St.InputField>
           <St.InputField>
             <St.P>휴대폰 번호</St.P>
             <St.PhoneField>
-              <St.Input
-                type="text"
-                placeholder="휴대폰 번호 '-' 제외하고 입력"
-              />
+              <Input type="text" placeholder="휴대폰 번호 '-' 제외하고 입력" />
               <St.Button>인증번호</St.Button>
             </St.PhoneField>
             <St.PhoneField>
-              <St.Input type="text" placeholder="인증번호 입력" />
+              <Input type="text" placeholder="인증번호 입력" />
               <St.Button>확인</St.Button>
             </St.PhoneField>
           </St.InputField>
           <St.Ul>
             <St.Li>
-              <St.Input type="checkbox" id="all-agree" />
+              <St.Checkbox type="checkbox" id="all-agree" />
               <label htmlFor="all-agree">전체동의</label>
             </St.Li>
             <hr />
             <St.Li>
-              <St.Input type="checkbox" id="terms-of-service" />
-              <label htmlFor="all-agree">서비스 이용약관 동의 (필수)</label>
+              <St.Checkbox type="checkbox" id="terms-of-service" />
+              <label htmlFor="terms-of-service">
+                서비스 이용약관 동의 (필수)
+              </label>
             </St.Li>
             <St.Li>
-              <St.Input type="checkbox" id="privacy-consent" />
-              <label htmlFor="all-agree">
+              <St.Checkbox type="checkbox" id="privacy-consent" />
+              <label htmlFor="privacy-consente">
                 개인정보 수집 및 이용 동의 (필수)
               </label>
             </St.Li>
