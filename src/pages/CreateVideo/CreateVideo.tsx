e8872a0cf71fe2afccd65a5ecb999d10cd27bf28
Haneul <skyhn0207@naver.com>
2023-06-25 11:17:13 +0900

    Feat: 강의만들기 기능 생성

--- src/pages/CreateVideo/CreateVideo.tsx
+++ src/pages/CreateVideo/CreateVideo.tsx
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
 import CreateNewVideo from 'components/CreateNewVideo/CreateNewVideo';
-import React from 'react';
+import CreateVideoTwo from 'components/CreateVideoTwo/CreateVideoTwo';
+
+export interface ButtonProps {
+  ChangePage: React.ButtonHTMLAttributes<HTMLButtonElement>
+  onClick: () => void;
+}
 
 const CreateVideo = () => {
+  const [createPage, setCreatePage] = useState<number>(1)
+  const ChangePage = () => {
+    setCreatePage(2)
+  }
   return (
     <div>
-      <CreateNewVideo/>
+      {
+        createPage === 1 ? <CreateNewVideo ChangePage={ChangePage}/> : <CreateVideoTwo/>
+      }
     </div>
   );
 };
