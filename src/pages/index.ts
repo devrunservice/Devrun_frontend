cb5adf4705699db3da96747b06dbf18c5914868a
hyejin <hejin8307@naver.com>
2023-07-02 16:34:04 +0900

    Feat: 로그인 리덕스 연동 (#9)

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -5,9 +5,8 @@ export { default as Login } from "./Login/Login";
 export { default as NotFound } from "./NotFound/NotFound";
 export { default as Notice } from "./Notice/Notice";
 export { default as Signup } from "./Signup/Signup";
-export {default as DetailPage} from "./DetailPage/DetailPage"
-export {default as CreateVideo} from "./CreateVideo/CreateVideo"
+export { default as DetailPage } from "./DetailPage/DetailPage";
+export { default as CreateVideo } from "./CreateVideo/CreateVideo";
 export { default as NoticeWrite } from "./NoticeWrite/NoticeWrite";
 export { default as NoticeDetail } from "./NoticeDetail/NoticeDetail";
-export { default as SignupSuccess } from "./Signup/SignupSuccess";
 export { default as FindId } from "./FindId/FindId";
