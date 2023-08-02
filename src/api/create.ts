6239812fe1810789a876697ac315f404dd17caa8
jinhwansong <119153742+jinhwansong@users.noreply.github.com>
2023-08-02 22:10:42 +0900

    fix : 충돌해결

--- src/api/create.ts
+++
@@ -1,9 +0,0 @@
-import { accAxios } from "./instance";
-import { CreateLectureType } from "types";
-
-export const createLecture = {
-  createLecture: (params:CreateLectureType) => {
-    const response = accAxios.post('/lectureresist', params)
-    return response
-  }
-}
