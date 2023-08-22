4aec78a6e745f19e466624fde494319378ffb1cb
Hyejin <31836035+hejin8307@users.noreply.github.com>
2023-08-22 22:43:36 +0900

    Update Recaptcha.tsx

--- src/components/Login/Recaptcha/Recaptcha.tsx
+++ src/components/Login/Recaptcha/Recaptcha.tsx
@@ -14,10 +14,10 @@ const Recaptcha = ({
   };
   return (
     <St.Section>
-      <ReCAPTCHA
+{/*       <ReCAPTCHA
         sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY as string}
         onChange={onChange}
-      />
+      /> */}
     </St.Section>
   );
 };
