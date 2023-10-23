05949abe92fa33146c9b146a7d14cb4a6d708fe4
jinhwansong <119153742+jinhwansong@users.noreply.github.com>
2023-10-23 23:21:23 +0900

    feat : app.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet, useLocation } from "react-router-dom";
 import {Footer, Header, MypageNav} from 'components';
-import MetaTag from 'utils/MetaTag';
+// import MetaTag from 'utils/MetaTag';
 import * as St from 'style/Common';
 
 const App = () => {
@@ -44,13 +44,13 @@ const App = () => {
 
   return (
     <>
-      <MetaTag
+      {/* <MetaTag
         content="개발, 강의, 포트폴리오, 공부, 링크, 링크모음"
         title="DevRun"
         description="개발자 공부를 위한 강의사이트 입니다. 강의결제, 강의등록등 여타 다른 사이트들과 같이 서비스를 이용해보세요"
         img=""
         url="/home"
-      />
+      /> */}
       {!hideHeader && <Header />}
       {!myPage ? (
         <Outlet />
